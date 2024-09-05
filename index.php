1960095b5233c099c37dcffeb90b909d31c4863b
LETUSOWNER <amansinghbillion@gmail.com>
2024-09-05 08:45:22 +0530

    index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Include the navbar
+include 'navbar.html';
+
+// Include the main card section
+include 'main.html';
+// Include the hover-box section
+include 'hover-box.html';
+//include the cards
+include 'cards.html';
+
+// Include the footer
+include 'footer.html';
+?>
